9b1526d987e9133deb08f428688c24cddddeed16
bofus10 <mauro90.07@gmail.com>
2021-05-17 00:05:44 -0300

    telegram getupdates done

--- telegram.py
+++ telegram.py
@@ -10,29 +10,52 @@ class Telegram():
 
   def __init__(self):
     self._offset = 0
+    self._query = {};
 
   def getUpdates(self,botID):
   
     url = "https://api.telegram.org/bot{}/getUpdates?offset={}".format(botID,self._offset)
     
     resp = json.loads((requests.request("GET", url)).text)
-    
+
     if resp["ok"] == True:
-      for msg in resp["result"]:
-        self._offset = msg["update_id"]
-        user_id = msg["message"]["from"]["id"]
-        username = msg["message"]["from"]["username"]
-        chat_id = msg["message"]["chat"]["id"]
-        text = msg["message"]["text"]
-        
-        print("{};{};{};{};{}".format(self._offset,user_id,username,chat_id,text))
-        
-        #Is command?
-        if "entities" in resp["result"]:
-          print("Is a command")
+      if len(resp["result"]) > 0:
+        for msg in resp["result"]:
+          self._offset = msg["update_id"]
+          self._query["user_id"] = msg["message"]["from"]["id"]
+          self._query["username"] = msg["message"]["from"]["username"]
+          self._query["chat_id"] = msg["message"]["chat"]["id"]
+          self._query["text"] = msg["message"]["text"]
           
+          #print("{};{};{};{};{}".format(self._offset,user_id,username,chat_id,text))
+
+          #Is command?
+          if "entities" in msg["message"]:
+            self._query["Iscommand"] = True
+          else:
+            self._query["Iscommand"] = False
+            
+          print(self._query)  
+            
+          #Was the last message? then we clean  
+          if msg == resp["result"][-1]:
+            print("this is the end")
+            url = "https://api.telegram.org/bot{}/getUpdates?offset={}".format(botID,self._offset+1)
+            requests.request("GET", url)
+            
+      else:
+        print("no message")
+        
+  #define simlpe mesage push
+  #def pushMSG(self,msg):
+  
+  
+  #def prepareResponse(self)
   
 
 
-#b1 = Telegram()
-#b1.getUpdates("BOT_TOKEN")
+b1 = Telegram()
+b1.getUpdates("")
+
+# get with default
+#student.get('subject', 'Science')
